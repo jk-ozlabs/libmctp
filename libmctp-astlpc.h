137fbceb45c462588105e4a646cb4863827ede0d
Jeremy Kerr <jk@ozlabs.org>
2019-03-01 12:18:07 +0800

    WIP: AST LPC binding implementation
    Signed-off-by: Jeremy Kerr <jk@ozlabs.org>

---
+++ libmctp-astlpc.h
@@ -0,0 +1,16 @@
+/* SPDX-License-Identifier: Apache-2.0 */
+
+#ifndef _LIBMCTP_ASTLPCL_H
+#define _LIBMCTP_ASTLPCL_H
+
+#include "libmctp.h"
+
+struct mctp_binding_astlpc;
+
+struct mctp_binding_astlpc *mctp_astlpc_init(void);
+int mctp_astlpc_get_fd(struct mctp_binding_astlpc *astlpc);
+void mctp_astlpc_register_bus(struct mctp_binding_astlpc *astlpc,
+		struct mctp *mctp, mctp_eid_t eid);
+int mctp_astlpc_poll(struct mctp_binding_astlpc *astlpc);
+
+#endif /* _LIBMCTP_ASTLPCL_H */
